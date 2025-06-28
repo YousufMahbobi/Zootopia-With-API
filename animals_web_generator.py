b01366daab28f9fb5f14509e7f72c09bce6e2362
Sayed Yousuf Mahbobi <sayedyousufmahbobi@Sayeds-MacBook-Air.local>
2025-06-28 17:13:46 +0200

    Animal API implemented and cleanup extra files implemented

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,24 +1,30 @@
-from data.data_service import load_data
+from data.data_service import fetch_animal_by_name
 from animals_file_management import read_html_template, write_html_content
 
-animals_data = load_data()
 
-fields_to_print = {
+FIELDS_TO_PRINT = {
     "characteristics_diet": lambda x: x['diet']  if isinstance(x, dict) and x.get('diet') else None,
     "locations": lambda x: x[0] if isinstance(x, list) and x else None,
     "characteristics_type": lambda x: x['type'] if isinstance(x, dict) and x.get('type') else None,
 }
 
-output = ''
+
+def prompt_animal_name():
+    return input("Enter animal's name: ")
 
 
-def serialize_animal(animal_obj):
-    output = ''
-    output += '<li class="cards__item">'
-    output += f'<div class="card__title">{animal_obj["name"]}</div>'
-    output += '<p class="card__text">'
+def search_animal_by_name(name):
+    return fetch_animal_by_name(name)
 
-    for field, handler in fields_to_print.items():
+
+def serialize_animal(animal):
+    html = [
+        '<li class="cards__item">'
+        f'<div class="card__title">{animal["name"]}</div>'
+        '<p class="card__text">'
+    ]
+
+    for field, handler in FIELDS_TO_PRINT.items():
         if '_' in field:
             field = field.split('_')
             base_key = field[0]
@@ -27,24 +33,52 @@ def serialize_animal(animal_obj):
             base_key = field
             actual_key = field
 
-        value = animal_obj.get(base_key)
+        value = animal.get(base_key)
         if value is not None:
             processed = handler(value)
             if processed:
-                output += f"<strong>{actual_key.title()}:</strong> {processed} <br/>"
+                html.append(f"<strong>{actual_key.title()}:</strong> {processed} <br/>")
 
-    output += '</p>'
-    output += '</li>\n'
-
-    return output
-
-for animal in animals_data:
-    output += serialize_animal(animal)
+    html += ['</p>', '</li>\n']
+    return ''.join(html)
 
 
+def serialize_animal_not_found(name):
+    return (
+        f'<h2 style="text-align: center;">Animal {name} not found.</h2>'
+        f'<h3 style="text-align: center;">Search another animal!</h2>'
+    )
 
 
-html_template = read_html_template()
-html_template = html_template.replace('__REPLACE_ANIMALS_INFO__', output)
-write_html_content(html_template)
+def serialze_animal_technical_error(error, status):
+    return (
+        f'<h2 style="text-align: center; font-size: 30px">Error_{status}</h2>'
+        f'<h3 style="text-align: center;">{error}</h2>'
+    )
+
+
+def generate_html_for_animals(animals, status, name):
+    if status == 200 and animals:
+        return ''.join(serialize_animal(animal) for animal in animals)
+    elif status == 200 and not animals:
+        return serialize_animal_not_found(name)
+    else:
+        return serialze_animal_technical_error(animals, status)
+
+
+def main():
+    name = prompt_animal_name()
+    animals, status, searched_name = search_animal_by_name(name)
+    html_body = generate_html_for_animals(animals, status, searched_name)
+
+    html_template = read_html_template()
+    html_template = html_template.replace('__REPLACE_ANIMALS_INFO__', html_body)
+    write_html_content(html_template)
+    print('Html created successfully.')
+
+
+if __name__ == '__main__':
+    main()
+
+
 
--- data/data_service.py
+++ data/data_service.py
@@ -1,5 +1,20 @@
-import json
+import requests
 
-def load_data():
-    with open('data/animals_data.json', 'r') as handle:
-        return json.load(handle)
+
+def fetch_animal_by_name(name):
+    api_url = "https://api.api-ninjas.com/v1/animals"
+    params = {"name": name}
+    headers = {
+        'X-Api-Key': 'M8A0LCi/SACfelo/+2S+3A==0Hr3zQtEbE9Arkpk'
+    }
+
+    response = requests.get(api_url, params=params, headers=headers)
+    if response.status_code == requests.codes.ok:
+        animals_info = response.json()
+        return animals_info, response.status_code, name
+    else:
+        return 'There is technical error, please contact website owner.', response.status_code, name
+
+
+
+
